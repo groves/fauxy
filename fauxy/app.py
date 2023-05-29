8177d0b09cc75c15739ddf2e93ffdaa2a5920486
Charlie Groves <charliegroves@datavant.com>
2023-05-29 16:36:25 -0400

    Use FileResponse and allow path to be given
    FileResponse will calculate content-length for us.
    A StreamingResponse of a meta without one will hang at the end as it
    doesn't know it's done.

--- fauxy/app.py
+++ fauxy/app.py
@@ -5,23 +5,20 @@ from fauxy.library import KeyMaker, Library, Recording
 from fauxy.record import HeaderProcessor, Recorder, RecordingProxy
 from starlette.applications import Starlette
 from starlette.requests import Request
-from starlette.responses import Response, StreamingResponse
+from starlette.responses import FileResponse, Response, StreamingResponse
 from starlette.routing import Route
 
 
-async def _create_streaming_response(rec: Recording) -> Response:
+async def _create_response(rec: Recording) -> Response:
     meta = json.loads(await (await rec.meta.open("r")).read())
-    return StreamingResponse(
-        await rec.response.open("rb"), meta["status"], meta["headers"]
-    )
+    return FileResponse(rec.response, meta["status"], meta["headers"])
 
 
-def create_proxy_route(handler: Callable[[Request], Awaitable[Response]]) -> Route:
+def create_proxy_route(
+    handler: Callable[[Request], Awaitable[Response]], path: str = "/{_:path}"
+) -> Route:
     # Proxy all paths, ignoring the filled in path param
-    # TODO let the path be specified to allow for different proxying behaviors for different paths
-    # Not sure if we should mix multiple key behaviors in a single dir and how to handle the path
-    # translation to the directory structure
-    r = Route("/{_:path}", handler)
+    r = Route(path, handler)
 
     # Allow matching on any method
     # Route fills in methods if we pass it None, so we set it to None to match all methods after init
@@ -36,7 +33,7 @@ def replay(library_dir: Path, key_maker: KeyMaker) -> Route:
 
     async def replay(req: Request) -> Response:
         if found := await library.find(req):
-            return await _create_streaming_response(found)
+            return await _create_response(found)
         return Response("No replay found for ", status_code=424)
 
     return create_proxy_route(replay)
@@ -46,9 +43,10 @@ def proxy(
     base_url: str,
     library_dir: Path,
     key_maker: KeyMaker,
-    header_processors: Optional[list[HeaderProcessor]] = None,
-    response_processors: Optional[list[Callable[[Response], None]]] = None,
-    disable_default_header_processors: Optional[bool] = False,
+    path: str = "/{_:path}",
+    header_processors: list[HeaderProcessor] | None = None,
+    response_processors: list[Callable[[Response], None]] | None = None,
+    disable_default_header_processors: bool | None = False,
 ) -> Route:
     library = Library(key_maker=key_maker, base_dir=library_dir)
 
@@ -62,10 +60,11 @@ def proxy(
 
     async def proxy_once(req: Request) -> Response:
         if found := await library.find(req):
-            return await _create_streaming_response(found)
+            resp = await _create_response(found)
+            return resp
         return await proxy.record(req)
 
-    return create_proxy_route(proxy_once)
+    return create_proxy_route(proxy_once, path)
 
 
 def app(routes: list[Route]) -> Starlette:
