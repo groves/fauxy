55398d31206da1ab78a5e682fb1a8a1434fdcb69
Charlie Groves <charliegroves@datavant.com>
2023-05-29 06:37:35 -0400

    Add an example of serving in-process that gets a port
    I used this inline to do it, but it's not ready as an API. Just noting
    it down for future reference before ripping it out in the next commit

--- fauxy/__init__.py
+++ fauxy/__init__.py
@@ -1,3 +1,4 @@
+import asyncio
 from typing import (
     Any,
     AsyncIterator,
@@ -5,7 +6,13 @@ from typing import (
     Optional,
     Tuple,
     TypeAlias,
+    Union,
 )
+import anyio
+
+from uvicorn import Config
+from uvicorn.server import Server
+from asgiref.typing import ASGIApplication
 
 from starlette.requests import Request
 from starlette.responses import Response
@@ -26,3 +33,15 @@ class ProxyOnce:
         if found := await self.library.find(req):
             return await self.library.serve(found)
         return await self.recorder.record(req)
+
+
+async def serve(app: Union[str, ASGIApplication, Callable]):
+    server = Server(Config(app, port=0))
+    with anyio.from_thread.start_blocking_portal() as bp:
+        bp.start_task_soon(server.serve)
+        while not server.started:
+            await asyncio.sleep(0.1)
+
+        port = server.servers[0].sockets[0].getsockname()[1]
+        yield port
+        server.should_exit = True
